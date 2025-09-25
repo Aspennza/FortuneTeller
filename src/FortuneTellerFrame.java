f4db282e8e939da6e4f692473aa7a3e5c60a3b6e
Zoe Aspenns <aspennza@mail.uc.edu>
2025-09-25 19:24:16 -0400

    Filled out part of createControlPnl(); added the Toolkit to allow for sizing the screen; created some fortune options

--- src/FortuneTellerFrame.java
+++ src/FortuneTellerFrame.java
@@ -1,7 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
 
 //make a list of the changes you still need to implement
+//Come back to writing fortunes into the ArrayList; return to the Swing GUI 02 video at 30 minutes
 //Write JavaDoc
 //UML diagrams?
 
@@ -21,10 +24,20 @@ public class FortuneTellerFrame extends JFrame
     JScrollPane scroller;
 
     JButton fortuneBtn;
+    ArrayList<String> fortuneOptions;
+
     JButton quitBtn;
 
     public FortuneTellerFrame()
     {
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        Dimension screenSize = kit.getScreenSize();
+        int screenHeight = screenSize.height;
+        int screenWidth = screenSize.width;
+
+        setSize(screenWidth * 3/4, screenHeight * 3/4);
+        setLocationRelativeTo(null);
+
         mainPnl = new JPanel();
         mainPnl.setLayout(new BorderLayout());
 
@@ -34,6 +47,17 @@ public class FortuneTellerFrame extends JFrame
         mainPnl.add(fortunesPnl, BorderLayout.CENTER);
 
         createControlPnl();
+
+        fortuneOptions.add("In the next two days, you will meet a lucky cat.");
+        fortuneOptions.add("In one month, you will encounter an ice cream truck.");
+        fortuneOptions.add("Within the next 5 months, you will be headbutted by a goat.");
+        fortuneOptions.add("In 5 years, you will eat a fortune cookie and receive your fortune.");
+        fortuneOptions.add("Three years from now, you will narrowly lose a bid for president of your local PTA.");
+        fortuneOptions.add("In 5 minutes, a fly will enter your house.");
+        fortuneOptions.add("In 30 seconds, you will continue to experience gravity.");
+        fortuneOptions.add("In the next two weeks, you will accidentally acquire a pizza.");
+        fortuneOptions.add("In 10 years, you will acquire a pet capybara.");
+        fortuneOptions.add("In 9 months, you will understand the meaning of E equals mc squared.");
 
         add(mainPnl);
         setTitle("Fortune Teller");
@@ -60,6 +84,19 @@ public class FortuneTellerFrame extends JFrame
 
     private void createControlPnl()
     {
+        controlPnl = new JPanel();
+        controlPnl.setLayout(new GridLayout(1, 2));
 
+        fortuneBtn = new JButton("Read My Fortune!");
+        quitBtn = new JButton("Quit");
+
+        controlPnl.add(fortuneBtn);
+        fortuneBtn.addActionListener((ActionEvent ae) ->
+        {
+
+        });
+
+        controlPnl.add(quitBtn);
+        quitBtn.addActionListener((ActionEvent ae) -> System.exit(0));
     }
 }
