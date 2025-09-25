a6f1f6b92bac0d7d2ee179f620375f746c4144f7
Zoe Aspenns <aspennza@mail.uc.edu>
2025-09-25 17:50:32 -0400

    Added a main panel, JTextArea, JScrollPane, and JButtons to FortuneTellerFrame; also added methods for creating the panels

--- src/FortuneTellerFrame.java
+++ src/FortuneTellerFrame.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 
 public class FortuneTellerFrame extends JFrame
 {
+    JPanel mainPnl;
     JPanel titlePnl;
     JPanel fortunesPnl;
     JPanel controlPnl;
@@ -9,10 +10,36 @@ public class FortuneTellerFrame extends JFrame
     JLabel titleLbl;
     ImageIcon fortuneIcon;
 
+    JTextArea fortuneTA;
+    JScrollPane scroller;
+
+    JButton fortuneBtn;
+    JButton quitBtn;
+
     public FortuneTellerFrame()
     {
+        mainPnl = new JPanel();
+        createTitlePnl();
+        createFortunesPnl();
+        createControlPnl();
+
         setTitle("Fortune Teller");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+    }
+
+    private void createTitlePnl()
+    {
+
+    }
+
+    private void createFortunesPnl()
+    {
+
+    }
+
+    private void createControlPnl()
+    {
+
     }
 }
