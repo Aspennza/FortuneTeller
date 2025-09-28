f4a3492388eb8f36bf7a6d773b97cf575d29e4e4
Zoe Aspenns <aspennza@mail.uc.edu>
2025-09-28 14:51:40 -0400

    Wrote javadoc for both programs

--- src/FortuneTellerFrame.java
+++ src/FortuneTellerFrame.java
@@ -4,48 +4,89 @@ import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import java.util.Random;
 
-//make a list of the changes you still need to implement
-//Come back to writing fortunes into the ArrayList; return to the Swing GUI 02 video at 30 minutes
-//Write JavaDoc
 //UML diagrams?
 
-public class FortuneTellerFrame extends JFrame
+/**
+ * Creates an extension of the JFrame class called FortuneTellerFrame. Allows users to press a button in the GUI
+ * to have a random fortune generated for them.
+ * @author Zoe Aspenns aspennza@mail.uc.edu
+ */
+public class FortuneTellerFrame extends javax.swing.JFrame
 {
+    //A JPanel containing all the other JPanels in the program
     JPanel mainPnl;
+
+    //A JPanel containing all the content aligned to the NORTH of the mainPnl
     JPanel titlePnl;
+
+    //A JPanel containing all the content aligned to the CENTER of the mainPnl
     JPanel fortunesPnl;
+
+    //A JPanel containing all the content aligned to the SOUTH of the mainPnl
     JPanel controlPnl;
 
+    //A Font used for all the text in the titlePnl
     Font titlePnlFont;
+
+    //A Font used for all the text in the fortunePnl
     Font fortunePnlFont;
+
+    //A Font used for all the text in the controlPnl
     Font controlPnlFont;
 
+    //A JLabel used to contain all the content in the titlePnl, including text and an ImageIcon
     JLabel titleLbl;
+
+    //An ImageIcon used to store the picture in the titlePnl
     ImageIcon fortuneIcon;
 
+    //A JTextArea used to display the randomized fortunes in the fortunesPnl
     JTextArea fortuneTA;
+
+    //A JScrollPane used to contain fortuneTA
     JScrollPane scroller;
 
+    //A JButton used to generate the randomized fortunes
     JButton fortuneBtn;
+
+    //An ArrayList used to store the list of all the fortunes
     ArrayList<String> fortuneOptions;
+
+    //This Random object allows for the random generation of ArrayList indices for fortunes
     Random gen = new Random();
+
+    //This int stores the current fortune index
     int fortuneIndex = 0;
-    String selectedFortune;
-    Integer previousIndex;
 
-    JButton quitBtn;
+    //This int stores the index of the previous fortune
+    int previousIndex = -1;
 
+    //This int counts the total number of fortunes that have been generated
     int fortuneCount = 0;
 
+    //This button allows the user to quit the GUI program
+    JButton quitBtn;
+
+    //This FortuneTellerFrame sets the title, size, visibility, and default close operation of FortuneTellerFrame; it also puts the main panel inside the JFrame and the other panels inside the main panel and establishes the fortune options
     public FortuneTellerFrame()
     {
+        super("Fortune Teller");
+
+        //This Toolkit is used to find the screen size of the computer running the GUI
         Toolkit kit = Toolkit.getDefaultToolkit();
+
+        //This Dimension stores the screen size
         Dimension screenSize = kit.getScreenSize();
+
+        //This int stores the height of the screen
         int screenHeight = screenSize.height;
+
+        //This int stores the width of the screen
         int screenWidth = screenSize.width;
 
         mainPnl = new JPanel();
         mainPnl.setLayout(new BorderLayout());
+        add(mainPnl);
 
         createTitlePnl();
         mainPnl.add(titlePnl, BorderLayout.NORTH);
@@ -72,14 +113,16 @@ public class FortuneTellerFrame extends JFrame
         fortuneOptions.add("In 1 week, you will meet a very old dog.");
         fortuneOptions.add("In three days, you will find an old piece of produce in the back of your fridge.");
 
-        add(mainPnl);
-        setTitle("Fortune Teller");
         setSize(screenWidth * 3/4, screenHeight * 3/4);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Fortune Teller");
         setVisible(true);
     }
 
+    /**
+     * This method establishes the title panel, sets the image and text that will go inside it, and declares the font it will use
+     */
     private void createTitlePnl()
     {
         titlePnl = new JPanel();
@@ -92,6 +135,9 @@ public class FortuneTellerFrame extends JFrame
         titlePnl.add(titleLbl);
     }
 
+    /**
+     * This method establishes the fortunes panel and its font, as well as the JTextArea and JScrollPane that will go inside it
+     */
     private void createFortunesPnl()
     {
         fortunesPnl = new JPanel();
@@ -104,6 +150,9 @@ public class FortuneTellerFrame extends JFrame
         fortunesPnl.add(scroller);
     }
 
+    /**
+     * This method establishes the control panel and its font, as well as the buttons and ActionListeners that go inside it
+     */
     private void createControlPnl()
     {
         controlPnl = new JPanel();
@@ -118,24 +167,24 @@ public class FortuneTellerFrame extends JFrame
         quitBtn.setFont(controlPnlFont);
 
         controlPnl.add(fortuneBtn);
+
+        //This algorithm means that when the fortuneBtn is clicked, the program will append a random fortune to the fortuneTA JTextArea
         fortuneBtn.addActionListener((ActionEvent ae) ->
         {
                 fortuneCount++;
 
-                fortuneIndex = gen.nextInt(14);
+                //This algorithm generates a random ArrayList index until it does not match the previous index
+                do {
+                    fortuneIndex = gen.nextInt(fortuneOptions.size());
+                } while (previousIndex == fortuneIndex);
 
-                while (previousIndex != null && previousIndex == fortuneIndex)
-                {
-                    fortuneIndex = gen.nextInt(14);
-                }
-
-                selectedFortune = fortuneOptions.get(fortuneIndex);
-
-                fortuneTA.append(fortuneCount + ") " + selectedFortune + "\n");
+                fortuneTA.append(fortuneCount + ") " + fortuneOptions.get(fortuneIndex) + "\n");
                 previousIndex = fortuneIndex;
         });
 
         controlPnl.add(quitBtn);
+
+        //This algorithm means that when the quitBtn is clicked, the program will terminate
         quitBtn.addActionListener((ActionEvent ae) -> System.exit(0));
     }
 }
--- src/FortuneTellerViewer.java
+++ src/FortuneTellerViewer.java
@@ -1,9 +1,17 @@
 import javax.swing.*;
 
+/**
+ * Creates a FortuneTellerFrame object so that the FortuneTellerFrame.java program can be tested and run.
+ * @author Zoe Aspenns aspennza@mail.uc.edu
+ */
 public class FortuneTellerViewer
 {
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args)
     {
+        //This JFrame creates an instance of the FortuneTellerFrame class
         JFrame frame = new FortuneTellerFrame();
     }
 }
